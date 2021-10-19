4e66644cd23cba22239b9846c9863de6dfd59648
Daniele Cordano <daniele.cordano@gmail.com>
2021-10-19 19:27:25 +0200

    Add auth

---
+++ src/components/AuthComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="auth">
+    <amplify-authenticator></amplify-authenticator>
+  </div>
+</template>
+<script>
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+export default {
+  name: "AuthComponent",
+  created() {
+    onAuthUIStateChange((nextAuthState, authData) => {
+      if (nextAuthState === AuthState.SignedIn) {
+        this.$router.go(-1);
+      }
+    });
+  },
+};
+</script>
+<style>
+.auth {
+  margin: 0 auto;
+  width: 460px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 import VueRouter from "vue-router";
 import CourseWithControls from "./components/CourseWithControls";
+import AuthComponent from "./components/AuthComponent";
+import ImportCourses from "./components/ImportCourses";
 import * as GmapVue from "gmap-vue";
+import "@aws-amplify/ui-vue";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 const opts = {
   icons: {
@@ -20,10 +27,14 @@ Vue.use(GmapVue, {
   installComponents: true
 });
 
-const routes = [{ path: "/", component: CourseWithControls }];
+const routes = [
+  { path: "/", component: CourseWithControls },
+  { path: "/auth", component: AuthComponent },
+  { path: "/import", component: ImportCourses }
+];
 
 const router = new VueRouter({
-  routes // short for `routes: routes`
+  routes
 });
 new Vue({
   router,
