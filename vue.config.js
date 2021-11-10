38d7bf93cfb68987848540e542ddfa1a06f7dbb1
Daniele Cordano <daniele.cordano@gmail.com>
2021-11-10 17:35:02 +0100

    Try fix build error

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,29 @@
-export const pwa = {
-  name: "Golfmaps",
-  themeColor: "#FFA500",
-  msTileColor: "#000000",
-  appleMobileWebAppCapable: "yes",
-  appleMobileWebAppStatusBarStyle: "#FFA500",
-  manifestOptions: {
+module.exports = {
+  pwa: {
     name: "Golfmaps",
-    short_name: "Golfmaps",
-    start_url: "/",
-    display: "standalone",
-    background_color: "#FFA500",
-    theme_color: "#FFA500",
-    orientation: "portrait-primary",
-    icons: [
-      {
-        src: "/public/golfmaps-512.png",
-        type: "image/png",
-        sizes: "512x512"
-      }
-    ]
-  },
-  iconPaths: {
-    favicon32: "/public/golfmaps.ico",
-    appleTouchIcon: "/public/golfmaps-512.png"
+    themeColor: "#FFA500",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "#FFA500",
+    manifestOptions: {
+      name: "Golfmaps",
+      short_name: "Golfmaps",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#FFA500",
+      theme_color: "#FFA500",
+      orientation: "portrait-primary",
+      icons: [
+        {
+          src: "/public/golfmaps-512.png",
+          type: "image/png",
+          sizes: "512x512"
+        }
+      ]
+    },
+    iconPaths: {
+      favicon32: "/public/golfmaps.ico",
+      appleTouchIcon: "/public/golfmaps-512.png"
+    }
   }
 };
